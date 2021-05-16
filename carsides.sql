9b11dd52e01ce0b04f5acecbf80f43b168351065
moratsam <vodopivec.rok@protonmail.com>
2021-05-16 10:14:38 +0200

    cleaning up fs

--- carsides.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE if not exists carsides;
-
-use carsides;
-create table if not exists images( id INT NOT NULL AUTO_INCREMENT, url TEXT NOT NULL, name VARCHAR(200) NOT NULL, PRIMARY KEY ( id ));
